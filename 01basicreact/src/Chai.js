2a272c05dabe6a608518ff1dbba49a013d8af4f2
Rahul Patel <rahulk77d@gmail.com>
2024-08-07 13:40:43 +0530

    understand react flow and structure

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>new js file is included and name of function is start with capital letter</h2>
+    )
+}
+export default Chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>new jsx file is included and name of the function is start with capital letter</h1>
+    )
+}
+
+export default Chai
