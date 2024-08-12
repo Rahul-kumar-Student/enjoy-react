fa9eeb7e2d8b0396c9261bbe061de222e054cbce
Rahul Patel <rahulk77d@gmail.com>
2024-08-12 20:44:27 +0530

    04/78

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let name="Harry";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Harry
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </nav>
+  <div className="container">
+    <h1>hello {name}</h1>
+    Lorem ipsum dolor sit amet consectetur
+       adipisicing
+       elit. Perspiciatis quia culpa voluptates
+        praesentium repellendus officiis neque repudiandae, 
+        voluptatibus perferendis quo id, iusto porro aliquam sequi
+       corporis, soluta necessitatibus! Fugit doloribus accusamus 
+       ratione provident animi?
+  </div>
+    </>
+   );
 }
 
 export default App;
