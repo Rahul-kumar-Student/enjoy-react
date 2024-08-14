37528692e61afe2fa2cb2efca6b57d33d7a4323d
Rahul Patel <rahulk77d@gmail.com>
2024-08-14 14:44:57 +0530

    07/78

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Navbar from './component/Navbar';
+import TextForm from './component/TextForm';
 function App() {
   return (
     <>
         <Navbar title="TextUtils" aboutText="About TextUtils"/>
+        <div className="container my-3">
+          <TextForm heading="Enter the text to analyze below"/>
+        </div>
     </>
    );
 }
