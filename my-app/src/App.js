08a8e49ae5432cd300989716116b678b4e3c3967
Rahul Patel <rahulk77d@gmail.com>
2024-08-14 10:41:09 +0530

    05/78

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,30 @@
 
 import './App.css';
-
 function App() {
-  let name="Harry";
   return (
     <>
-  <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-  </nav>
-  <div className="container">
-    <h1>hello {name}</h1>
-    Lorem ipsum dolor sit amet consectetur
-       adipisicing
-       elit. Perspiciatis quia culpa voluptates
-        praesentium repellendus officiis neque repudiandae, 
-        voluptatibus perferendis quo id, iusto porro aliquam sequi
-       corporis, soluta necessitatibus! Fugit doloribus accusamus 
-       ratione provident animi?
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
+</nav>
     </>
    );
 }
