f38beac9a7a0ce3068fde03834e921c0dfc5bf58
Rahul Patel <rahulk77d@gmail.com>
2024-08-14 12:13:52 +0530

    06/78

---
+++ my-app/module1.mjs
@@ -0,0 +1,8 @@
+import dza, {a,d,c} from './module2.mjs'
+console.log(dza);
+console.log(c);
+console.log(d);
+console.log(a);
+
+
+   
---
+++ my-app/module2.mjs
@@ -0,0 +1,9 @@
+const a="Harry";
+const b="rohan";
+const c="Akash";
+const d="priyanka";
+
+export default b;
+export {a};
+export {c};
+export {d};
