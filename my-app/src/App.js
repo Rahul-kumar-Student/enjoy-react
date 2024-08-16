2399ecfe54e5f65cfc2d5f899e736db3bfa6a63c
Rahul Patel <rahulk77d@gmail.com>
2024-08-16 14:25:27 +0530

    10/78

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
 import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
+// import TextForm from './component/TextForm';
+import About from './component/About';
 function App() {
   return (
     <>
         <Navbar title="TextUtils" aboutText="About TextUtils"/>
         <div className="container my-3">
-          <TextForm heading="Enter the text to analyze below"/>
+         {/* <TextForm heading="Enter the text to analyze below"/> */}
+         <About/>
         </div>
     </>
    );
 }
 
 export default App;
+ 
